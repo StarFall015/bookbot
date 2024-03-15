e70c4e88b5298ffb7be63080fe4c866559311690
StarFall015 <kieranbradbury015@hotmail.com>
2024-03-15 01:28:59 +0000

    made pathing for obtaining text easier

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 def main():
-    text = get_book_text()
+    text = get_book_text("frankenstein")
     count_words(text)
 
 
-def get_book_text():
-    with open("books/frankenstein.txt") as f:
+def get_book_text(book):
+    with open(f"books/{book}.txt") as f:
         return f.read()
 
 
 def count_words(file):
     words = file.split()
     print(len(words))
+
+
+def count_letters():
+
 
 
 main()
